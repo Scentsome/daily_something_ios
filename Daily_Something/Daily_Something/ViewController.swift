006b8fc7c0b79d17a62d87ee8226549ad3817f1d
Michael Pan <scentsome@gmail.com>
2015-08-11 14:43:23 +0800

    add var name

--- Daily_Something/Daily_Something/ViewController.swift
+++ Daily_Something/Daily_Something/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var name = "Michael"
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
