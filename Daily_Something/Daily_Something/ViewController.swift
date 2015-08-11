f54e39d014234a6b97176eac63d1b364389c1dbd
Michael Pan <scentsome@gmail.com>
2015-08-11 14:38:41 +0800

    add a new project

---
+++ Daily_Something/Daily_Something/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Daily_Something
+//
+//  Created by mac on 2015/8/11.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
